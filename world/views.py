cd2fd568dc8083e59da6e2c552f3503970f28e35
WiktoriaTomczyk <autoryzowana@gmail.com>
2017-03-19 07:44:33 -0700

    Stworzenie mapy

--- world/views.py
+++ world/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import WorldBorder
+from django.core.serializers import serialize
 
-# Create your views here.
+def index(request, fips):
+    border = WorldBorder.objects.filter(fips=fips.upper())
+    fields=('name', 'area', 'pop2005', 'region', 'subregion', 'lon', 'lat')
+    geojson = serialize('geojson', border, geometry_field='geom', fields=fields)
+    return render(request, 'world/index.html', { 'border': geojson })
+
